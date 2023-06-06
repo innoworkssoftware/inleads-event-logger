ee9ca74380169bd26e3897bac96ca04f7b82642c
Nagendra <>
2023-06-06 19:20:45 +0200

    Added functionality for setUser

--- dist/events.d.ts
+++ dist/events.d.ts
@@ -1,2 +1,3 @@
 export declare function init(apiKey: string): Promise<void>;
-export declare function track(eventName: string, email: string, name?: string): Promise<void>;
+export declare function setUser(email: string, name?: string, options?: any): Promise<void>;
+export declare function track(eventName: string, options?: any): Promise<void>;
--- dist/events.js
+++ dist/events.js
@@ -12,10 +12,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.track = exports.init = void 0;
+exports.track = exports.setUser = exports.init = void 0;
 const js_cookie_1 = __importDefault(require("js-cookie"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const COOKIE_KEY = 'inleads-event-key';
+const COOKIE_EMAIL_KEY = 'inleads-event-email';
+const COOKIE_NAME_KEY = 'inleads-event-name';
+const COOKIE_OPTIONS_KEY = 'inleads-event-options';
 const COOKIE_LENGTH = 365;
 let Fetch = fetch;
 if (typeof fetch === 'undefined') {
@@ -43,13 +46,39 @@ function init(apiKey) {
     });
 }
 exports.init = init;
-function track(eventName, email, name) {
+function setUser(email, name, options = {}) {
+    return __awaiter(this, void 0, void 0, function* () {
+        js_cookie_1.default.remove(COOKIE_EMAIL_KEY);
+        js_cookie_1.default.remove(COOKIE_NAME_KEY);
+        js_cookie_1.default.remove(COOKIE_OPTIONS_KEY);
+        js_cookie_1.default.set(COOKIE_EMAIL_KEY, email, {
+            expires: COOKIE_LENGTH,
+        });
+        js_cookie_1.default.set(COOKIE_OPTIONS_KEY, options, {
+            expires: COOKIE_LENGTH,
+        });
+        if (name) {
+            js_cookie_1.default.remove(COOKIE_NAME_KEY);
+            js_cookie_1.default.set(COOKIE_NAME_KEY, name, {
+                expires: COOKIE_LENGTH,
+            });
+        }
+    });
+}
+exports.setUser = setUser;
+function track(eventName, options = {}) {
     return __awaiter(this, void 0, void 0, function* () {
         const eventCookie = js_cookie_1.default.get(COOKIE_KEY);
+        const email = js_cookie_1.default.get(COOKIE_EMAIL_KEY);
+        const name = js_cookie_1.default.get(COOKIE_NAME_KEY);
+        const userOptions = js_cookie_1.default.get(COOKIE_OPTIONS_KEY);
         if (!eventCookie) {
             throw new Error(`uh oh!, looks like you haven't called the init method`);
         }
-        if (!eventName || !email) {
+        if (!email) {
+            throw new Error(`uh oh!, looks like you haven't called the setUser method`);
+        }
+        if (!eventName) {
             throw new Error(`Missing required information.`);
         }
         try {
@@ -59,6 +88,8 @@ function track(eventName, email, name) {
                     eventName,
                     email,
                     name,
+                    options,
+                    userOptions,
                     apiKey: eventCookie,
                 }),
                 headers: {
