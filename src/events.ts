6a54def487321058ad869dc6ba404f5f665997cf
Nagendra <>
2023-06-18 21:14:35 +0200

    prettified

--- src/events.ts
+++ src/events.ts
@@ -21,26 +21,34 @@ if (typeof fetch === 'undefined') {
 
 function generateUUID() {
   var d = new Date().getTime();
-  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16;//random number between 0 and 16
-    if(d > 0){//Use timestamp until depleted
-      r = (d + r)%16 | 0;
-      d = Math.floor(d/16);
+  var d2 =
+    (typeof performance !== 'undefined' &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
     } else {
-      r = (d2 + r)%16 | 0;
-      d2 = Math.floor(d2/16);
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
     }
-    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
 }
 
 async function generateMeta() {
   try {
     var ua = Parser(navigator.userAgent);
-    const clientResponse = await Fetch(`${BIG_DATA_CLOUD_ENDPOINT}/client-info`, {
+    const clientResponse = await Fetch(
+      `${BIG_DATA_CLOUD_ENDPOINT}/client-info`,
+      {
       method: 'get',
-    });
+      },
+    );
     const response = await Fetch(
       `${BIG_DATA_CLOUD_ENDPOINT}/reverse-geocode-client`,
       { method: 'get' },
@@ -155,7 +163,7 @@ export async function track(eventName: string, options: any = {}) {
         options,
         userOptions: userOptions ? JSON.parse(userOptions) : {},
         apiKey: eventCookie,
-        sessionKey
+        sessionKey,
       }),
       headers: {
         'Content-Type': 'application/json',
